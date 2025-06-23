f8276057820d47697a7710002c70351d28ff226c
ekutyebu <ekuty2001@gmail.com>
2025-06-23 20:36:50 +0100

    creating all basic files for both frontend and backend

--- hustlr_backend/accounts/apps.py
+++ hustlr_backend/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HustlrAppConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hustlr_app'
+    name = 'accounts'
